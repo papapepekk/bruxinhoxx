4fd9ae7f0f50a26f73014bcd27c6a3fbe74c9910
bruxinhok <77681118+bruxinhok@users.noreply.github.com>
2021-02-15 19:05:53 -0300

    Add files via upload

---
+++ FADHIL/help.js
@@ -0,0 +1,42 @@
+const help = (prefix, instagram, yt, name, pushname2, user, limitt) => { 
+	return `
+
+╭───────────────────
+┃╭─────────────────
+┃├➲ \`\`\`Salve\`\`\`👋 *${pushname2}*
+┃├➲ \`\`\`Eu sou\`\`\` *${ARR157-BOT}
+┃├➲ \`\`\`Seu limite :\`\`\` *${limitt}*
+┃├➲ \`\`\`Verifikasi.   :\`\`\` *SS*
+┃├➲ \`\`\`Premium.   :\`\`\` *NN*
+┃├➲ \`\`\`Total de usuários :\`\`\` *${user.length} User*
+┃├➲ \`\`\`Total Donasi :\`\`\` *0%* 🙂
+┃╰─────────────────
+┃
+┃╭───「 *LIST MENU* 」───
+┃├❍ *${prefix}allmenu*
+┃├❍ *${prefix}mediamenu*
+┃├❍ *${prefix}creatormenu*
+┃├❍ *${prefix}groupmenu*
+┃├❍ *${prefix}makermenu*
+┃├❍ *${prefix}funmenu*
+┃├❍ *${prefix}nsfwmenu*
+┃├❍ *${prefix}onwermenu*
+┃╰─────────────────
+┃
+┃╭─────「 *ABOUT* 」───
+┃├❍ *${prefix}info*
+┃├❍ *${prefix}donasi*
+┃├❍ *${prefix}owner*
+┃├❍ *${prefix}speed*
+┃├❍ *${prefix}daftar*
+┃├❍ *${prefix}totaluser*
+┃├❍ *${prefix}chatlist*
+┃├❍ *${prefix}grouplist*
+┃├❍ *${prefix}blocklist*
+┃├❍ *${prefix}banlist*
+┃├❍ *${prefix}bahasa*
+┃╰────────────────
+
+         *© BRUXINHO MODS*`
+}
+exports.help = help
