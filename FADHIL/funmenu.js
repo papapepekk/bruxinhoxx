0fc95aa007591d53efea3b02ea85c322e5d3f91e
papapepekk <77702436+papapepekk@users.noreply.github.com>
2021-02-16 18:02:49 -0300

    Update funmenu.js

--- FADHIL/funmenu.js
+++ FADHIL/funmenu.js
@@ -1,43 +1,265 @@
-const funmenu = (prefix) => { 
-	return ` 
-╭───「 *FUN & GAME* 」──
-│
-├➲ *${prefix}anjing*
-├➲ *${prefix}kucing*
+const listmenu = (prefix) => { 
+	return ` *LIST MENU FADHIL BOT*
+	
+<══════════════════════>
+               CREITIM
+ <══════════════════════>
+
+__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█▄█████▄█   Author:gusta
+__█▼▼▼▼▼█ 
+_██ᏴϴͲ Ꮩ4.0██▌ 
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+
+┌─────────4.0────────┐
+└─────────4.0────────┘
+╔══════════════════════
+║ ᏟϴᎷᎪΝᎠϴՏ ᏞᏆՏͲᎪᎠϴՏ
+╠══════════════════════
+╭─「 *MEDIA 」──
+├➲ *${prefix}tiktokstalk [usuario]
+├➲ *${prefix}igstalk [ahmd.fdhl_] *
+├➲ *${prefix}insta [Link]*
+├➲ *${prefix}instastory [usuario]
+├➲ *${prefix}ssweb [url]*
+├➲ *${prefix}url2img [Url]*
+├➲ *${prefix}tiktok*
+├➲ *${prefix}fototiktok*
+├➲ *${prefix}meme*
+├➲ *${prefix}memeindo*
+├➲ *${prefix}kbbi*
+├➲ *${prefix}wait*
+├➲ *${prefix}trendtwit*
+├➲ *${prefix}google [ultimas notícias]*
+├➲ *${prefix}pinterest*
+├➲ *${prefix}play*
+╰─────────────────────
+
+╭────「 *DIVERSÃO ────
+├➲ *${prefix}anjing* (cachorro)
+├➲ *${prefix}kucing* (gato)
 ├➲ *${prefix}testime*
-├➲ *${prefix}hilih*
+├➲ *${prefix}hilih* (escolher )
 ├➲ *${prefix}say*
-├➲ *${prefix}apakah*
-├➲ *${prefix}kapankah*
-├➲ *${prefix}bisakah*
-├➲ *${prefix}rate*
-├➲ *${prefix}watak*
+├➲ *${prefix}apakah* (é)
+├➲ *${prefix}kapankah* (quando)
+├➲ *${prefix}bisakah* (você pode)
+├➲ *${prefix}rate* (avaliar)
+├➲ *${prefix}watak* 
 ├➲ *${prefix}hobby*
-├➲ *${prefix}infogempa*
-├➲ *${prefix}infonomor* 
-├➲ *${prefix}quotes*
-├➲ *${prefix}truth*
-├➲ *${prefix}dare*
-├➲ *${prefix}katabijak*
-├➲ *${prefix}fakta*
-├➲ *${prefix}darkjokes*
-├➲ *${prefix}bucin*
-├➲ *${prefix}pantun*
-├➲ *${prefix}katacinta*
-├➲ *${prefix}jadwaltvnow*
-├➲ *${prefix}hekerbucin*
-├➲ *${prefix}katailham*
-├➲ *${prefix}caklontong*
+├➲ *${prefix}infogempa* (info terremoto)
+├➲ *${prefix}infonomor* (informação do número)
+├➲ *${prefix}fakta* (facto)
+├➲ *${prefix}darkjokes* (piadas negras)
+├➲ *${prefix}katacinta* (palavras de amor)
+├➲ *${prefix}jadwaltvnow* (agendar tv agora)
+├➲ *${prefix}hekerbucin* (hacker bucin)
+├➲ *${prefix}katailham* (disse inspiração)
 ├➲ *${prefix}family100*
+├➲ *${prefix}asupan* (ingestão)
+├➲ *${prefix}family100*
+├➲ *${prefix}kalkulator [13*12]*
+├➲ *${prefix}wp [Montanha]
+╰─────────────────────
+
+
+╭───「 *OUTROS 」──
+├➲ *${prefix}translate [en / Como vai você?] *
+├➲ *${prefix}pasangan [Fadhil/Salsa]*
+├➲ *${prefix}gantengcek [Fadhil]*
+├➲ *${prefix}cantikcek [Salsa]*
+├➲ *${prefix}artinama [Fadhil]*
+├➲ *${prefix}persengay [Bryan]*
+├➲ *${prefix}pbucin [Fadhil]*
+├➲ *${prefix}bpfont [Fadhil]*
+├➲ *${prefix}textstyle [Seu texto]
+├➲ *${prefix}jadwaltv [GTV]*
+├➲ *${prefix}lirik [I am lady]*
+├➲ *${prefix}chord [I am lady]*
+├➲ *${prefix}wiki [Adolf Hitler]*
+├➲ *${prefix}brainly [pesquisa]*
+├➲ *${prefix}resepmasakan [Rawn]*
+├➲ *${prefix}map [Klaten]*
+├➲ *${prefix}film [The Walking Death]*
+├➲ *${prefix}pinterest [fotos de gatos]*
+├➲ *${prefix}infocuaca [Klaten]*
+├➲ *${prefix}jamdunia [Indonesia]*
+├➲ *${prefix}mimpi [Com ele] *
+├➲ *${prefix}infoalamat [jalan Klaten]*
 ├➲ *${prefix}asupan*
 ├➲ *${prefix}tebakgambar*
 ├➲ *${prefix}caklontong*
 ├➲ *${prefix}family100*
 ├➲ *${prefix}kalkulator [13*12]*
 ├➲ *${prefix}wp [gunung]*
-│
-╰───────────────────
-	
-               *© BRUXINHO MODS*`
-	}
-exports.funmenu = funmenu
+├➲ *${prefix}jadwalsholat [Klaten]*
+├➲ *${prefix}quran*
+├➲ *${prefix}quransurah [1]*
+├➲ *${prefix}tafsir [1/5]*
+├➲ *${prefix}becrypt [string]*
+├➲ *${prefix}encode64 [string]*
+├➲ *${prefix}decode64 [encrypt]*
+├➲ *${prefix}encode32 [string]*
+├➲ *${prefix}decode32 [encrypt]*
+├➲ *${prefix}encbinary [string]*
+├➲ *${prefix}decbinary [encrypt]*
+├➲ *${prefix}encoctal [string]*
+├➲ *${prefix}decoctal [encrypt]*
+├➲ *${prefix}hashidentifier [Encrypt Hash]*
+├➲ *${prefix}dorking [dork]*
+├➲ *${prefix}pastebin [teks]*
+├➲ *${prefix}tinyurl [link]*
+├➲ *${prefix}bitly [link]*
+╰─────────────────────
+
+╭───「 *OUTROS*  ───
+├➲ *${prefix}sticker*
+├➲ *${prefix}quotemaker [tx/wtrmk/tema]*
+├➲ *${prefix}nulis [nama/kelas/text]*
+├➲ *${prefix}trigger [reply image]*
+├➲ *${prefix}rip [reply image]*
+├➲ *${prefix}wasted [reply image]*
+├➲ *${prefix}cphlogo [Fadhil/Graphy]*
+├➲ *${prefix}cglitch [Fadhil/Graphy]*
+├➲ *${prefix}cpubg [Fadhil/Graphy]*
+├➲ *${prefix}cml [Fadhil/Graphy]*
+├➲ *${prefix}tahta [Salsa]*
+├➲ *${prefix}croman [Fadhil dan Salsa]*
+├➲ *${prefix}cthunder [Fadhil Graphy]*
+├➲ *${prefix}cbpink [Fadhil Graphy]*
+├➲ *${prefix}cmwolf [Fadhil Graphy]*
+├➲ *${prefix}csky [Fadhil Graphy]*
+├➲ *${prefix}cwooden [Fadhil Graphy]*
+├➲ *${prefix}cflower [Fadhil Graphy]*
+├➲ *${prefix}clove [Fadhil Graphy]*
+├➲ *${prefix}ccrossfire [Fadhil Graphy]*
+├➲ *${prefix}cnaruto [Fadhil Graphy]*
+├➲ *${prefix}cparty [Fadhil Graphy]*
+├➲ *${prefix}cshadow [Fadhil Graphy]*
+├➲ *${prefix}cminion [Fadhil Graphy]*
+├➲ *${prefix}cneon [Fadhil Graphy]*
+├➲ *${prefix}cneon2 [Fadhil Graphy]*
+├➲ *${prefix}cneongreen [Fadhil Graphy]*
+├➲ *${prefix}c3d [Fadhil Graphy]*
+├➲ *${prefix}csky [Fadhil Graphy]*
+├➲ *${prefix}tts [id Haii]*
+├➲ *${prefix}ttp [Fadhil Graphy]*
+├➲ *${prefix}slide [Fadhil Graphy]*
+├➲ *${prefix}stiker*
+├➲ *${prefix}gifstiker*
+├➲ *${prefix}toimg*
+├➲ *${prefix}img2url*
+├➲ *${prefix}nobg*
+├➲ *${prefix}tomp3*
+├➲ *${prefix}ocr*
+╰──────────────────────
+
+╭──────「 * GRUPO*  」───
+├➲ *${prefix}modeanime [On/Off]*
+├➲ *${prefix}naruto*
+├➲ *${prefix}minato*
+├➲ *${prefix}boruto*
+├➲ *${prefix}hinata*
+├➲ *${prefix}sakura*
+├➲ *${prefix}sasuke*
+├➲ *${prefix}kaneki*
+├➲ *${prefix}toukachan*
+├➲ *${prefix}rize*
+├➲ *${prefix}akira*
+├➲ *${prefix}itori*
+├➲ *${prefix}kurumi*
+├➲ *${prefix}miku*
+├➲ *${prefix}anime*
+├➲ *${prefix}animecry*
+├➲ *${prefix}neonime*
+├➲ *${prefix}animekiss*
+├➲ *${prefix}wink*
+├➲ *${prefix}welcome [On/Off]*
+├➲ *${prefix}grup [buka/tutup]*
+├➲ *${prefix}antilink [on/off*
+├➲ *${prefix}ownergrup*
+├➲ *${prefix}setpp*
+├➲ *${prefix}infogc*
+├➲ *${prefix}add*
+├➲ *${prefix}kick*
+├➲ *${prefix}promote*
+├➲ *${prefix}demote*
+├➲ *${prefix}setname*
+├➲ *${prefix}setdesc*
+├➲ *${prefix}linkgrup*
+├➲ *${prefix}tagme*
+├➲ *${prefix}hidetag*
+├➲ *${prefix}tagall*
+├➲ *${prefix}mentionall*
+├➲ *${prefix}fitnah*
+├➲ *${prefix}listadmin*
+├➲ *${prefix}openanime*
+├➲ *${prefix}edotense*
+├➲ *${prefix}nsfw [On/Off]*
+├➲ *${prefix}nsfwloli*
+├➲ *${prefix}nsfwblowjob*
+├➲ *${prefix}nsfwneko*
+├➲ *${prefix}nsfwtrap*
+├➲ *${prefix}hentai*
+├➲ *${prefix}simih [On/Off]*
+╰──────────────────────
+
+╭────「 PROPRIETÁRIO* ✓────
+├➲ *${prefix}addprem [mentioned]*
+├➲ *${prefix}removeprem [mention]*
+├➲ *${prefix}setppbot*
+├➲ *${prefix}setreply*
+├➲ *${prefix}bc*
+├➲ *${prefix}bcgc*
+├➲ *${prefix}ban*
+├➲ *${prefix}unban*
+├➲ *${prefix}block*
+├➲ *${prefix}unblock*
+├➲ *${prefix}clearall*
+├➲ *${prefix}delete*
+├➲ *${prefix}clone*
+├➲ *${prefix}getses*
+├➲ *${prefix}leave*
+╰──────────────────────
+
+
+╭─────「 *MENU PREMIUM* 」───
+├➲ *${prefix}playmp3 [I am Lady]*
+├➲ *${prefix}fb [link video]*
+├➲ *${prefix}snack [link snack video]*
+├➲ *${prefix}ytmp3 [link yt]*
+├➲ *${prefix}ytmp4 [link yt]*
+├➲ *${prefix}joox [Ultimate Monologue]
+├➲ *${prefix}smule [Link Video Smule]*
+├➲ *${prefix}cersex*
+├➲ *${prefix}asupan*
+├➲ *${prefix}xxx [japan]*
+├➲ *${prefix}pornhub 
+├➲ *${prefix}hentai [Random]*
+├➲ *${prefix}ban [TagUser]*
+├➲ *${prefix}unban [TagUser]*
+├➲ *${prefix}bc [teks]*
+├➲ *${prefix}asupan*
+╰──────────────────────
+            CREITIM BOT😼
+   ╲    ╱   
+       ╱▔▔▔▔▔╲       
+      ┃┈▇┈┈▇┈┃      
+╭╮┣━━━━━━┫╭╮    
+┃┃┃┈┈┈┈┈┈┃┃┃    
+╰╯┃┈┈┈┈┈┈┃╰╯
+      ╰┓┏━━┓┏╯
+         ╰╯      ╰╯
+┌─────────4.0────────┐
+         SHITS_MANGA_ROSA 
+└─────────4.0────────┘
+
+`
+} 
+exports.listmenu = listmenu
+
+
+
+
